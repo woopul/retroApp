c97cff7e99aa35ae1c714c77f67324510e6ade29
Wojtek Opulski <wojtekopulski@gmail.com>
2023-09-21 01:49:09 +0200

    feat: cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
-import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import Link from 'next/link';
 
-import { api } from '@/utils/api';
-
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: 'from tRPC' });
-
   return (
     <>
       <Head>
@@ -26,38 +21,8 @@ export default function Home() {
             <h3 className="text-2xl font-bold">Test Draggable →</h3>
             <div className="text-lg">Just some poc of basic draggable functionality</div>
           </Link>
-          <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-              {hello.data ? hello.data.greeting : 'Loading tRPC query...'}
-            </p>
-            <AuthShowcase />
-          </div>
         </div>
       </main>
     </>
-  );
-}
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined },
-  );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? 'Sign out' : 'Sign in'}
-      </button>
-    </div>
   );
 }
